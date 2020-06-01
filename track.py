fe3df08cc66da278867c9bd26cc2bd34fcc1eb9b
Caleb Vatral <caleb.m.vatral@lions.enc.edu>
2020-06-01 14:26:00 -0500

    Add minimum working example

---
+++ track.py
@@ -0,0 +1,34 @@
+import numpy as np
+from mf_sort import MF_SORT, Detection
+from tqdm import trange
+
+detections_path = "example_data/overpass_detections.txt"
+output_path = "example_data/overpass_track.txt"
+
+all_dets = np.loadtxt(detections_path, delimiter=",")
+frame_range = int(np.max(all_dets[:,0]))
+
+mot = MF_SORT(max_age=5, min_hits=3, gating_threshold=0.7)
+tracks_by_frame = []
+
+for frame_num in trange(frame_range, unit="frames"):
+    # Extract detections from this frame
+    dets = all_dets[all_dets[:, 0] == frame_num]
+    dets = [Detection(r[2:6], r[6]) for r in dets]
+
+    # Run the tracker
+    mot.predict()
+    trackers = mot.update(dets) # Returns tuples of (mf_sort.Detection, trk_num)
+
+    # Format output
+    for trk, trk_num in trackers:
+        x1 = int(trk.tlwh[0])
+        y1 = int(trk.tlwh[1])
+        w = int(trk.tlwh[2])
+        h = int(trk.tlwh[3])
+
+        trk_out = (frame_num, x1, y1, w, h, trk_num)
+        tracks_by_frame.append(trk_out)
+
+trks = np.array(tracks_by_frame)
+np.savetxt(output_path, trks, delimiter=",", fmt="%d")
---
+++ visualize_track.py
@@ -0,0 +1,47 @@
+import numpy as np
+import cv2
+from tqdm import trange
+import imutils.video
+
+video_path = "example_data/overpass.mp4"
+tracks_path = "example_data/overpass_track.txt"
+output_path = "example_data/overpass_track.avi"
+
+# Load the tracks generated from MF-SORT
+tracks = np.loadtxt(tracks_path, delimiter=",", dtype="int")
+num_tracks = tracks[:, 5].max()
+COLORS = np.random.randint(0, 255, size=(num_tracks+1, 3), dtype="int")
+
+# Setup Video File
+vs = cv2.VideoCapture(video_path)
+fourcc = cv2.VideoWriter_fourcc(*"MJPG")
+W, H = None, None
+writer = None
+frame_total = imutils.video.count_frames(video_path)
+
+# Loop through video fames with progress bar
+for frame_number in trange(1, frame_total+1, unit="frames"):
+    grabbed, frame = vs.read()
+    if not grabbed:
+        break
+    if W is None or H is None:
+        H, W = frame.shape[:2]
+    if writer is None:
+        writer = cv2.VideoWriter(output_path, fourcc, 15, (W, H), True)
+
+    # Extract the Tracks for the current frame
+    frame_tracks = tracks[tracks[:, 0] == frame_number]
+    for trk in frame_tracks:
+        _, x, y, w, h, trk_ID = trk
+
+        # Draw a bounding box rectangle and label
+        color = tuple(int(i) for i in COLORS[trk_ID]) # openCV is picky about data format for color
+        text = "Tracker {}".format(trk_ID)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), color, 2)
+        cv2.putText(frame, text, (x, y-5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+    # Write the frame to file
+    writer.write(frame)
+
+writer.release()
+vs.release()
